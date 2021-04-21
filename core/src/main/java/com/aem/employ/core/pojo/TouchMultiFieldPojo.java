27944b97829e69ce6c0560886bbbda77ceb270f8
Kalinga@20 <mailtoanreddy@gmail.com>
2021-04-21 09:18:32 +0530

    first commit

---
+++ core/src/main/java/com/aem/employ/core/pojo/TouchMultiFieldPojo.java
@@ -0,0 +1,31 @@
+package com.aem.employ.core.pojo;
+
+import java.util.HashMap;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.adobe.cq.sightly.WCMUsePojo;
+
+public class TouchMultiFieldPojo extends WCMUsePojo {
+
+	
+	private static final Logger log = LoggerFactory.getLogger(TouchMultiFieldPojo.class);
+	  
+	//Logger log=LoggerFactory.getLogger(this.getClass());
+	private HashMap<String,String> links=new HashMap<>();
+	
+	public void activate() throws Exception {
+		String linkpath[]=getProperties().get("linkpath",String[].class);
+		String linktitle[]=getProperties().get("linktitle",String[].class);
+		for(int i=0;i<linktitle.length;i++) {
+			links.put(linktitle[i],linkpath[i]);
+		}
+		log.info("links: "+links);
+	}
+	public HashMap<String, String> getLinks() {
+		return links;
+	}
+	
+	
+}
